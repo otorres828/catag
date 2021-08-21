7971ba40c08d94116c64a57ff0b2dd65f1c3b790
Oliver English <52586453+otorres828@users.noreply.github.com>
2021-08-21 04:27:51 -0400

    datatable traslate

--- Admin/producto.php
+++ Admin/producto.php
@@ -100,7 +100,29 @@
 
     <script>
         $(document).ready(function() {
-            $('#example').DataTable();
+            $('#example').DataTable({
+                "language": {
+                "lengthMenu": "Mostrar "+
+                            `<select>
+                            <option value ='10'>10</option>
+                            <option value ='25'>25</option>
+                            <option value ='50'>50</option>
+                            <option value ='100'>100</option>
+                            <option value ='-1'>ALL</option>
+                            </select>`+
+                            " registro por pagina",
+                "zeroRecords": "Nada encontrado",
+                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
+                "infoEmpty": "No records available",
+                "infoFiltered": "(filtrado de _MAX_ registros totales)",
+                "search": "Buscar:",
+                "paginate":{
+                    'next':'siguiente',
+                    'previous':'anterior'
+                }
+                }
+            });
+            
         });
     </script>
 
